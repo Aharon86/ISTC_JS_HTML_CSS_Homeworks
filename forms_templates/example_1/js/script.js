124a680d821b592b1688b558e42c3a11a48024f2
Aharon <aharonhg@gmail.com>
2019-01-26 02:33:22 +0400

    change forms templates

--- forms_templates/example_1/js/script.js
+++ forms_templates/example_1/js/script.js
@@ -8,20 +8,20 @@ sign_in.addEventListener('click', changeSignIn);
 
 function changeSignUp() {
     sign_up.classList = 'active';
-    form1.style.zIndex = '2';
+    form1.style.display = 'block';
     sign_in.classList = 'not_active';
-    form2.style.zIndex = '1';
+    form2.style.display = 'none';
 }
 
 function changeSignIn() {
     sign_up.classList = 'not_active';
-    form2.style.zIndex = '2';
+    form2.style.display = 'block';
     sign_in.classList = 'active';
-    form1.style.zIndex = '1';
+    form1.style.display = 'none';
 }
 
 // input validation
-var input = form_1.getElementsByTagName('input');
+var input = document.getElementsByTagName('input');
 var regExp = {
     name: /^[a-z]+$/i,
     surname: /^[a-z]+$/i,
@@ -31,12 +31,18 @@ var regExp = {
     confirm_password: /^\w{6,14}$/i
 }
 var validInput = {
-    name: {valid: false, err: 'The name must have at least 1 letter'},
-    surname: {valid: false, err: 'The surname must have at least 1 letter'},
-    email: {valid: false, err: 'Pl. enter a correct email ex."exam@ex.com"'},
-    username: {valid: false, err: 'The username must have at least 1 letteraa'},
-    password: {valid: false, err: 'Password must be from 6 to 14 characters'},
-    confirm_password: {valid: false, err: 'Please enter same password'}
+        sign_up: {
+            name: {valid: false, err: 'The name must have at least 1 letter'},
+            surname: {valid: false, err: 'The surname must have at least 1 letter'},
+            email: {valid: false, err: 'Pl. enter a correct email ex."exam@ex.com"'},
+            username: {valid: false, err: 'The username must have at least 1 letteraa'},
+            password: {valid: false, err: 'Password must be from 6 to 14 characters'},
+            confirm_password: {valid: false, err: 'Please enter same password'}
+        },
+        sign_in: {
+            username: {valid: false, err: 'The username must have at least 1 letteraa'},
+            password: {valid: false, err: 'Password must be from 6 to 14 characters'}
+        }
 }
 var myValue = {};
 
@@ -45,45 +51,52 @@ for (let i = 0; i < input.length; i++) {
 }
 
 //  validation function
-function validatorInput(event, inpOut) {
-    var inp;
+function validatorInput(event, inpOut, objKey) {
+    var inp, keyValidInput;
 
     if (inpOut) {
         inp = inpOut;
+        keyValidInput = objKey;
     } else {
         inp = this;
+        if (inp.parentElement.parentElement.id == 'form_1' ) {
+            keyValidInput = 'sign_up'; 
+        }else{
+            keyValidInput = 'sign_in'; 
+        }
+        
     }
     
     if (regExp[inp.name].test(inp.value)) {
         if (inp.name == 'confirm_password') {
             if (inp.value == myValue.password) {
-                setCorrect(inp);
+                setCorrect(inp, keyValidInput);
             }else{
-                setWrong(inp);
+                setWrong(inp, keyValidInput);
             }
         }else{
-            setCorrect(inp);
+            setCorrect(inp, keyValidInput);
         }
     } else {
-        setWrong(inp);
+        setWrong(inp, keyValidInput);
     }
     // console.log(regExp[inp.name].test(inp.value));
     // console.log(myValue );
 }
 // validation style and get any values
-function setCorrect(inp) {
+function setCorrect(inp, keyValidInput) {
     inp.style.borderBottom = '2px solid green';
     inp.style.background = 'url("image/iconfinder_check.png") no-repeat right center';
-    validInput[inp.name].valid = true;
+    validInput[keyValidInput][inp.name].valid = true;
     myValue[inp.name] = inp.value;
     inp.nextElementSibling.innerHTML = '';
 }
-function setWrong(inp) {
+function setWrong(inp, keyValidInput) {
     inp.style.borderBottom = '2px solid red';
-    inp.style.background = 'url("image/invalid.png") no-repeat right center';
-    validInput[inp.name].valid = false;
+    inp.style.background = 'none';
+    validInput[keyValidInput][inp.name].valid = false;
     myValue[inp.name] = inp.value;
-    inp.nextElementSibling.innerHTML = validInput[inp.name].err;
+    inp.nextElementSibling.innerHTML = validInput[keyValidInput][inp.name].err;
 }
 function clearValueStyle(inp) {
     inp.removeAttribute("style");
@@ -92,22 +105,29 @@ function clearValueStyle(inp) {
 
 // The last check, sending data and receiving from the server
 
-form_1.getElementsByTagName('button')[0].addEventListener('click', sendObj);
+form1.getElementsByTagName('button')[0].addEventListener('click', sendObj);
+form2.getElementsByTagName('button')[0].addEventListener('click', sendObj);
 
 function sendObj() {
+    var kayValidInput;
+    if (this.parentElement.id == 'form_1' ) {
+        keyValidInput = 'sign_up'; 
+    }else{
+        keyValidInput = 'sign_in'; 
+    }
     // write errors
-    for (var key in validInput) {
-        validatorInput(event, document.getElementById(key).getElementsByTagName('input')[0]);
+    for (var key in validInput[keyValidInput]) {
+        validatorInput(event, this.parentElement.getElementsByClassName(key)[0] , keyValidInput );
     }
     // breake or not
-    for (var key in validInput) {
-        if (!validInput[key].valid) {
+    for (var key in validInput[keyValidInput]) {
+        if (!validInput[keyValidInput][key].valid) {
             return;
         }
     }
     // if all values are correct return standard styles
-    for (var key in validInput) {
-        clearValueStyle(document.getElementById(key).getElementsByTagName('input')[0]);
+    for (var key in validInput[keyValidInput]) {
+        clearValueStyle(this.parentElement.getElementsByClassName(key)[0]);
     }
 
     fetch("http://rest.learncode.academy/api/johnbob/friends", {
