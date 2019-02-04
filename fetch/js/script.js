d6ee8f0fcbe011763112349227af4777f6d835ae
Aharon <aharonhg@gmail.com>
2019-02-04 23:40:16 +0400

    finish fetch

--- fetch/js/script.js
+++ fetch/js/script.js
@@ -1,18 +1,20 @@
-function getObj() {
+/* receiving data from the server */
+function getObj() {                                    
     fetch("https://jsonplaceholder.typicode.com/todos")
       .then(res => res.json())
-      .then(response => writeData(response))
+      .then(response => writeData(response))                    // call writeData() function to display data
       .catch(error => console.error('Error:', error));
 }
 
- var ul = document.getElementsByTagName('ul')[0];
+var ul = document.getElementsByTagName('ul')[0];        // get 'ul' element 
 
+/*  */
 function writeData(res) {
     for (let i = 0; i < 5; i++) {
         var li = document.createElement('li');
-        li.innerHTML += res[i].userId + '---';
-        li.innerHTML += res[i].title + '---';
-        li.innerHTML += res[i].completed
+        for (let key in res[i]) {
+            li.innerHTML += key + ': ' + res[i][key] + '; ';
+        }
         ul.appendChild(li);
     }
     console.log(res);
