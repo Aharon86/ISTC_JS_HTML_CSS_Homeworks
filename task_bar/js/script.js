ea2d64c69b0062711ab000450bbece394076c215
Aharon <aharonhg@gmail.com>
2019-02-05 01:04:41 +0400

    change task_bar

--- task_bar/js/script.js
+++ task_bar/js/script.js
@@ -7,6 +7,7 @@ var iconsClass = {                                          // object for icons
         check: 'far fa-check-square',
         trash: 'fas fa-trash-alt'
     };
+var arrElements = [];                                      //block tasks
 
 /* function to add tasks */
 function addToDoList() {
@@ -22,21 +23,18 @@ function addToDoList() {
     taskList.unshift( {task: inp.value, made: false} );    // adding front
     inp.value = '';                                        // clearing input field
     createTaskTable(taskList);                             // call function creating 
-
-    // console.log(taskList);
-
 }
+
 
 /* creating a task table */
 function createTaskTable(list) {
     section.innerHTML = '';                         // clearing 'section' block
 
+    arrElements = [];
+
     /* creating previous task section and its nested items */
     for (let i = 0; i < list.length; i++) {
         var block = document.createElement('div');
-
-        var span = document.createElement('span');
-        span.appendChild(document.createTextNode(i));
 
         var text = document.createElement('p');
         text.appendChild(document.createTextNode(list[i].task));
@@ -47,6 +45,11 @@ function createTaskTable(list) {
         var iconTrash = document.createElement('i');
         iconTrash.className = iconsClass.trash;
 
+        block.appendChild(iconCheck);
+        block.appendChild(text);
+        block.appendChild(iconTrash);
+        section.appendChild(block);
+
         /* checking tasks done or not done, and overeating the appropriate style */
         if (list[i].made) {                          
             block.className = 'done'; 
@@ -56,24 +59,22 @@ function createTaskTable(list) {
         }
         iconTrash.addEventListener('click', deleteTask);
 
-        block.appendChild(span);
-        block.appendChild(iconCheck);
-        block.appendChild(text);
-        block.appendChild(iconTrash);
-        section.appendChild(block);
+        arrElements.push(block);                                       
         
     }
-    
+
+    // console.log(arrElements);
 }
+
 /* if done it is marked true */
 function taskDone(event) {
-    var taskId = event.target.parentElement.children[0].innerHTML;
+    var taskId = arrElements.indexOf(event.target.parentElement);
     taskList[taskId].made = true;
     createTaskTable(taskList);
 }
 /* delete task*/
 function deleteTask(event) {
-    var taskId = event.target.parentElement.children[0].innerHTML;
+    var taskId = arrElements.indexOf(event.target.parentElement);
     taskList.splice(taskId, 1);
     createTaskTable(taskList);
 }
